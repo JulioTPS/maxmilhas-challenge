2e5e6401471ce20f4fe0322499bb7a7c77208b70
Júlio Tetsuji Pinheiro So <so.julio@hotmail.com>
2023-10-22 17:56:41 -0300

    works

---
+++ src/main/java/com/maxmilhas/MaxmilhasApplication.java
@@ -0,0 +1,13 @@
+
+package com.maxmilhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxmilhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxmilhasApplication.class, args);
+	}
+}
